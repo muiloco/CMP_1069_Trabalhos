e8f4a670f8a374a5a89a11ac2fdeb5f10b66934b
muiloco <muiloco17@gmail.com>
2018-12-12 10:34:47 -0200

    strategy finalizado

--- ProjetoStrategy/src/main/Main.java
+++ ProjetoStrategy/src/main/Main.java
@@ -22,20 +22,7 @@ public class Main {
     
     public static void main(String[] args) {
         // TODO code application logic here
-        Moedas dolar = new Moedas("EUA", (float)3.94);
-        Moedas euro = new Moedas("UE", (float)4.47);
-        Moedas iene = new Moedas("JAP", (float)0.035);
-        
-        float real = (float) 35.84;
-        DecimalFormat df = new DecimalFormat("0.00");
-        
-        String saida1 = "Valor em Dolar=" + df.format(dolar.converterMoeda(real));
-        String saida2 = "Valor em Euro=" + df.format(euro.converterMoeda(real));
-        String saida3 = "Valor em Iene=" + df.format(iene.converterMoeda(real));
-        
-        System.out.println(saida1);
-        System.out.println(saida2);
-        System.out.println(saida3);
+        Tela.main(null);
     }
     
 }
